b9e84325930c5f923df2875f678a56b9c7f69b55
Jihun <90238606+paichaiboy@users.noreply.github.com>
2025-02-15 19:37:36 +0900

    Create chat_screen.dart

---
+++ waylo_flutter/lib/screens/main/chat_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChatScreenPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('채팅 페이지'),
+        ),
+      ),
+    );
+  }
+}
