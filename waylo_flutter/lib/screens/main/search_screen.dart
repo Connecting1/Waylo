62526af6289336951982f7b454cc1dc8c74151d7
Jihun <90238606+paichaiboy@users.noreply.github.com>
2025-02-15 19:39:15 +0900

    Create search_screen.dart

---
+++ waylo_flutter/lib/screens/main/search_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchScreenPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('검색 페이지'),
+        ),
+      ),
+    );
+  }
+}
