b96161d6bf9087df6fca88b6d77207efdb277a32
Jihun <90238606+paichaiboy@users.noreply.github.com>
2025-02-15 19:18:23 +0900

    Create models.py

---
+++ waylo_api/users/models.py
@@ -0,0 +1,41 @@
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    # password = models.TextField(null=True, blank=True)
+    gender = models.CharField(max_length=20, choices=[
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+        ('non-binary', 'Non-binary'),
+        ('prefer not to say', 'Prefer not to say'),
+    ])
+    username = models.CharField(max_length=30, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    provider = models.CharField(max_length=50, default='local')
+    profile_image = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    last_login = models.DateTimeField(null=True, blank=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'users'
+
+class CustomToken(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="custom_auth_token")
+    key = models.CharField(max_length=40, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = uuid.uuid4().hex  
+        return super().save(*args, **kwargs)
