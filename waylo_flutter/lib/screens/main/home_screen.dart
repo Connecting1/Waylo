d7ed52b75276f17ac6bd176c85f4342b6976952c
Jihun <90238606+paichaiboy@users.noreply.github.com>
2025-02-15 19:37:58 +0900

    Create home_screen.dart

---
+++ waylo_flutter/lib/screens/main/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('홈 페이지'),
+        ),
+      ),
+    );
+  }
+}
