f66e35a89afd0e9304e5484ff49ae9728501155b
Jihun <90238606+paichaiboy@users.noreply.github.com>
2025-02-27 15:28:35 +0900

    Update urls.py

--- waylo_api/waylo_api/urls.py
+++ waylo_api/waylo_api/urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),  # users 앱의 URL을 포함
+    path('admin/', admin.site.urls),  # Django Admin
+    path('api/users/', include('users.urls')),  # users API 통합
+    path('api/albums/', include('albums.urls')),  # albums API 통합
 ]
+
+# 미디어 파일 제공 (DEBUG=True일 때만 적용됨)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
