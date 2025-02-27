807858225ac64d6fdb57e35e1adde3045f3238fd
Jihun <90238606+paichaiboy@users.noreply.github.com>
2025-02-27 15:11:15 +0900

    Update models.py

--- waylo_api/albums/models.py
+++ waylo_api/albums/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 import uuid
-from users.models import User  # ✅ 사용자 모델 임포트
+from users.models import User  # 사용자 모델 임포트
 
 class Album(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="album")
-    background_color = models.CharField(max_length=10, default="#FFFFFF")  # ✅ 배경 색상 추가
-    background_pattern = models.CharField(max_length=50, default="none")  # ✅ 배경 패턴 추가
+    background_color = models.CharField(max_length=10, default="#FFFFFF")  # 배경 색상 추가
+    background_pattern = models.CharField(max_length=50, default="none")  # 배경 패턴 추가
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
