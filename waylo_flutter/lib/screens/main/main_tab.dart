c38d1dc85b0ea57967df52d47071a3c3bf85adb8
Jihun <90238606+paichaiboy@users.noreply.github.com>
2025-02-27 15:39:15 +0900

    Update main_tab.dart

--- waylo_flutter/lib/screens/main/main_tab.dart
+++ waylo_flutter/lib/screens/main/main_tab.dart
@@ -1,71 +1,55 @@
 import 'package:flutter/material.dart';
-import 'home_screen.dart';
+import 'my_map_screen.dart';
 import 'chat_screen.dart';
-import 'upload_screen.dart';
+import 'shared_map_screen.dart';
 import 'search_screen.dart';
-import 'profile_screen.dart';
+import 'album_screen.dart';
 import '../../styles/app_styles.dart'; // 스타일 파일 import
 
-class MainTabPage extends StatelessWidget {
+class MainTabPage extends StatefulWidget {
   const MainTabPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary), // 스타일에서 색상 사용
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MainTabPage> createState() => _MainTabPageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+class _MainTabPageState extends State<MainTabPage> with SingleTickerProviderStateMixin {
   TabController? controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Waylo'),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            HomeScreenPage(),
-            ChatScreenPage(),
-            UploadScreenPage(),
-            SearchScreenPage(),
-            ProfileScreenPage(),
-          ],
-          controller: controller,
-        ),
-        bottomNavigationBar: TabBar(
-          tabs: <Tab>[
-            Tab(icon: Icon(Icons.home, color: AppColors.primary)),
-            Tab(icon: Icon(Icons.chat, color: AppColors.primary)),
-            Tab(icon: Icon(Icons.edit, color: AppColors.primary)),
-            Tab(icon: Icon(Icons.search, color: AppColors.primary)),
-            Tab(icon: Icon(Icons.person, color: AppColors.primary)),
-          ],
-          controller: controller,
-        ));
-  }
 
   @override
   void initState() {
     super.initState();
     controller = TabController(length: 5, vsync: this);
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: TabBarView(
+          physics: NeverScrollableScrollPhysics(),
+          children: <Widget>[
+            MyMapScreenPage(),
+            AlbumScreenPage(),
+            SharedMapScreenPage(),
+            SearchScreenPage(),
+            ChatScreenPage(),
+          ],
+          controller: controller,
+        ),
+        bottomNavigationBar: TabBar(
+          tabs: <Tab>[
+            Tab(icon: Icon(Icons.person_pin_circle)),
+            Tab(icon: Icon(Icons.auto_stories)),
+            Tab(icon: Icon(Icons.public)),
+            Tab(icon: Icon(Icons.search)),
+            Tab(icon: Icon(Icons.forum)),
+          ],
+          controller: controller,
+          labelColor: AppColors.primary,       // 선택된 탭 아이콘 색상
+          unselectedLabelColor: Colors.grey,   // 선택되지 않은 탭 아이콘 색상
+          indicatorColor: AppColors.primary,
+        ));
   }
 
   @override
