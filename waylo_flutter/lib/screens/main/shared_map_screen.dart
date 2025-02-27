2a7fcf28580c0101f75e0e32e3423c8188761cad
Jihun <90238606+paichaiboy@users.noreply.github.com>
2025-02-27 15:43:11 +0900

    Create shared_map_screen.dart

---
+++ waylo_flutter/lib/screens/main/shared_map_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SharedMapScreenPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('공유맵 페이지'),
+        ),
+      ),
+    );
+  }
+}
