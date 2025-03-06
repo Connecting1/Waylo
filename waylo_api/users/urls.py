0549eeb7ac8873ddafa5d4625c65b2b165412549
Jihun <90238606+paichaiboy@users.noreply.github.com>
2025-03-06 12:02:20 +0900

    Update urls.py

--- waylo_api/users/urls.py
+++ waylo_api/users/urls.py
@@ -3,8 +3,8 @@ from .views import (
     user_create_view,
     user_login_view,
     get_user_info,
-    # upload_profile_image,
-    update_user_info
+    update_user_info,
+    update_profile_image
 )
 
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
     path('login/', user_login_view, name='user-login'),  # 로그인 API
     path('<uuid:user_id>/', get_user_info, name='get_user_info'),  # 유저 정보 조회 (GET)
     path('<uuid:user_id>/update/', update_user_info, name='update_user_info'),  # 유저 정보 업데이트 (PATCH)
+    path('<str:user_id>/update-profile-image/', update_profile_image, name='update_profile_image'),
 ]
