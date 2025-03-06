a8338f217df34346d4546b33c70eb9afc242945e
Jihun <90238606+paichaiboy@users.noreply.github.com>
2025-03-06 11:58:38 +0900

    Update urls.py

--- waylo_api/albums/urls.py
+++ waylo_api/albums/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_album_info  # views에서 get_album_info 가져오기
+from .views import get_album_info, update_album_info  # views에서 get_album_info 가져오기
 
 urlpatterns = [
-    path('api/albums/<uuid:user_id>/', get_album_info, name='get_album_info'),  # UUID로 앨범 조회
+    path('<uuid:user_id>/', get_album_info, name='get_album_info'),  # UUID로 앨범 조회
+    path('<uuid:user_id>/update/', update_album_info, name='update_album_info'),  # 유저 정보 업데이트 (PATCH)
 ]
