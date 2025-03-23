f2d1db8620489a2fd5823a9548eb4edf4469e6d0
Jihun <90238606+paichaiboy@users.noreply.github.com>
2025-03-23 23:46:29 +0900

    Delete waylo_api/albums/apps.py

--- waylo_api/albums/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AlbumsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'albums'
-
-    def ready(self):
-        import albums.signals  # signals.py import
